4aded0d508903963d7e6390efffa247e6c310e93
songshaohua <rudy@atytec.com>
2020-07-18 12:00:00 +0800

    更改init方法

--- 脚手架/rdy-cli/README.md
+++ 脚手架/rdy-cli/README.md
@@ -1,6 +1,14 @@
-## ry-cli
-1. rycli add # add template
-2. rycli delete template-name # delete template
-3. rycli list # check all template list
-4. rycli init template-name url # init project 
+## rdy-cli 项目脚手架
+<!-- 1. rdycli add # add template
+2. rdycli delete template-name # delete template
+3. rdycli list # check all template list
+4. rdycli init template-name url # init project  -->
 
+1. 需要添加模板 ` rdycli add `,然后会让你填写模板名称，及模板地址
+
+
+2. 添加自己所需的模板之后，可以去初始项目了。`rdycli init`，在填写提示的一些参数之后，初始项目会从`template.json`中拉去所需要的模板资源, 这里会有点慢，请耐心等候
+PS：这里gitlab 如果是有ip组成，默认使用node 自带 `exec`去下载，否则使用 `download-git-repo`远程下载
+
+3. `rdycli delete` ，填写模板名称，删除模板
+4. `rdycli list` ，查看所有模板名
--- 脚手架/rdy-cli/bin/rdycli-init.js
+++ 脚手架/rdy-cli/bin/rdycli-init.js
@@ -5,64 +5,115 @@ const ora = require('ora')
 const tpls = require(`${__dirname}/../template`)
 const tip = require('../lib/tip')
 const exec = require('child_process').exec
+const download = require('download-git-repo')
+const inquirer = require('inquirer')
+
 
 program
   .usage('<template-name> [project-name]')
 
-  program.parse(process.argv)
-
-  if (program.args.length < 1) return program.help()
-
-let templateName = program.args[0]
-let projectName = program.args[1]
-
-if (!tpls[templateName]) {
-  tip.fail('Template should not be empty!')
-  return
-}
-
-if (!projectName) {
-  tip.fail('Project should not be empty!')
-  return
-}
-
-url = tpls[templateName]
-
-tip.suc('Start generating...')
-
-const spinner = ora('Download...')
-
-const execRm = (err, projectName) => {
-  spinner.stop()
-  if (err) {
-    console.log(err)
-    tip.fail('请重新运行!')
-    process.exit()
+let question = [
+  {
+    name: 'templateName',
+    type: 'input',
+    message: '请输入模板名称',
+    validate(val) {
+      if (val === '') {
+        return '请输入模板名称'
+      } else if (!tpls[val]) {
+        return '模板不存在'
+      } else {
+        return true
+      }
+    }
+  },
+  {
+    name: 'projectName',
+    type: 'input',
+    message: '请输入新建文件夹名称',
+    validate(val) {
+      if (val === '') {
+        return '文件夹不能为空'
+      } else {
+        return true
+      }
+    }
+  },
+  {
+    name: 'type',
+    type: 'list',
+    message: '请输入仓库地址类型',
+    choices: ['github', 'gitlab'],
+    validate(val) {
+      type = val
+    }
   }
+]
 
-  tip.suc('初始化完成!')
-  tip.info(`cd ${projectName} && npm install`)
-  process.exit()
-}
+inquirer
+  .prompt(question).then(answers => {
+    let { templateName, type, projectName } = answers
+    let url = tpls[templateName]
+    tip.suc('Start generating...')
 
-const download = (err, projectName) => {
-  if (err) {
-    console.log(err)
-    tip.fail('请重新运行')
-    process.exit()
-  }
+    const spinner = ora('Download...')
 
-  exec(`cd ${projectName} && rm -rf .git`, (err, out) => {
-    execRm(err, projectName)
+    if (type === 'gitlab' && url.indexOf('gitlab') === -1) {
+      const execRm = (err, projectName) => {
+        spinner.stop()
+        if (err) {
+          console.log(err)
+          tip.fail('请重新运行!')
+          process.exit()
+        }
+
+        tip.suc('初始化完成!')
+        tip.suc(`cd ${projectName} && npm install`)
+        process.exit()
+      }
+
+      const downloadProject = (err, projectName) => {
+        if (err) {
+          console.log(err)
+          tip.fail('请重新运行')
+          process.exit()
+        }
+
+        exec(`cd ${projectName} && rm -rf .git`, (err, out) => {
+          execRm(err, projectName)
+        })
+      }
+
+      const cmdStr = `git clone ${url} ${projectName} && cd ${projectName}`
+
+      spinner.start()
+
+      exec(cmdStr, (err) => {
+        downloadProject(err, projectName)
+      })
+    } else {
+      const prefix = type === 'gitlab' ? 'gitlab' : 'direct'
+      url = `${prefix}:${url}`
+      spinner.start()
+      // 执行下载方法并传入参数
+      download(
+        url,
+        projectName,
+        {
+          clone: true
+        },
+        err => {
+          if (err) {
+            spinner.fail();
+            tip.fail(`Generation failed. ${err}`)
+            return
+          }
+          // 结束加载图标
+          spinner.succeed();
+          tip.suc('Generation completed!')
+          tip.suc('To get started')
+          tip.suc(`cd ${projectName}`)
+        }
+      )
+    }
   })
-}
-
-const cmdStr = `git clone ${url} ${projectName} && cd ${projectName}`
-
-spinner.start()
-
-console.log('正在下载中...')
-
-exec(cmdStr, (err) => {
-  download(err, projectName)
-})
